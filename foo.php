7ecda47c754e241bb0e8b72cf7c76d94aff560ba
sambowne <sbowne@ccsf.edu>
2019-05-16 12:44:28 -0700

    Add files via upload

--- foo.php
+++ foo.php
@@ -3,7 +3,7 @@
 print 1;
 
 include 'poss_chals.php';
-# if (! isset($poss_chals) ) {
+if (! isset($poss_chals) ) { print 2; }
 # 	print 'Error: poss_chals not set in poss_chals.php<p>':
 # 	exit;
 # }
